dd514fc15100b072601855a57ffa15eacd6e40c2
Nik <nn494@yandex.ru>
2020-12-25 19:17:16 +0300

    Added map generation

---
+++ Board.py
@@ -0,0 +1,50 @@
+import numpy as np
+import noise
+import random
+
+
+class Board:
+    def __init__(self, width=3, height=3, left=0, top=0, cell_size=30):
+        self.width = width
+        self.height = height
+        self.board = np.zeros((height, width), dtype=np.uint8)
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def set_view(self, left, top, cell_size=None):
+        self.left = left
+        self.top = top
+        if not None:
+            self.cell_size = cell_size
+
+    def render(self, screen):
+        pass
+
+    def get_cell(self, mouse_pos):
+        pos_x, pos_y = (mouse_pos[0] - self.left) // self.cell_size, \
+                       (mouse_pos[1] - self.top) // self.cell_size
+        if self.width > pos_x > -1 and self.height > pos_y > -1:
+            return pos_x, pos_y
+        return None
+
+    def on_click(self, cell_coords):
+        if cell_coords is not None:
+            x, y = cell_coords
+            self.board[y][x] = 1
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        self.on_click(cell)
+
+    def generate_world(self):
+        base = random.randint(0, 100000000)
+        for i in range(self.height):
+            for j in range(self.width):
+                noise_val = noise.pnoise2(j / self.width, i / self.height, 6, 0.5, 2, base)
+                if noise_val < -0.05:
+                    self.board[i][j] = 0
+                elif noise_val < 0:
+                    self.board[i][j] = 1
+                else:
+                    self.board[i][j] = 2
